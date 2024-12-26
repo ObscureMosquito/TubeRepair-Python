364c39a0a13de472605c6a8b2bc5fd54dc75ce58
Obscure Mosquito <130766178+ObscureMosquito@users.noreply.github.com>
2024-12-26 23:25:03 +0100

    Deleted unecesary and old files

--- TubeRepair.h
+++ TubeRepair.h
@@ -1,11 +1,4 @@
-// Global variables for Client ID and Secret
-extern NSString *globalClientID;
-extern NSString *globalClientSecret;
-
 // Function declarations
-void extractClientIDAndSecretFromScript(NSString *scriptContent, void(^completion)(BOOL success, NSString *clientID, NSString *clientSecret));
-void fetchYouTubeTVPageAndExtractClientID(void(^completion)(BOOL success, NSString *clientID, NSString *clientSecret));
 void refreshOAuthTokenIfNeeded(void);
 void scheduleTokenRefresh(void);
-void addCustomHeaderToRequest(NSMutableURLRequest *request);
 void betaSetDefaultUrl(void);
